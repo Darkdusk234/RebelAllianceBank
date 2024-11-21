c0e75a05eb8a6439034ea7f9c0eb392c3869ab4d
darkdusk234 <johan.svensson.01@hotmail.com>
2024-11-21 14:22:22 +0100

    Added username and password variables to IUser interface

--- RebelAllianceBank/Classes/Customer.cs
+++ RebelAllianceBank/Classes/Customer.cs
@@ -3,6 +3,8 @@ namespace RebelAllianceBank.Classes
 {
     public class Customer : IUser
     {
+        public string Username { get; set; }
+        public string Password { get; set; }
 
     }
 }
