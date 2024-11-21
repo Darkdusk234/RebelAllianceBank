476e9f9be3cf164ad4c4a955faa51fc501e94433
darkdusk234 <johan.svensson.01@hotmail.com>
2024-11-21 15:06:52 +0100

    Created Login method

--- RebelAllianceBank/Classes/Bank.cs
+++ RebelAllianceBank/Classes/Bank.cs
@@ -3,11 +3,66 @@ namespace RebelAllianceBank.Classes
 {
     public class Bank
     {
-        List<IUser> users = new List<IUser>() { new Admin() };
+        List<IUser> users = new List<IUser>() { new Admin("FullAccessLogin", "02492512") };
+        IUser currentUser;
 
-        public static void Run()
+        public void Run()
         {
 
+        }
+
+        public void Login()
+        {
+            while (true)
+            {
+                Console.WriteLine("Välkommen till Rebel Alliance Bank. Vänligen skriv ditt användarnamn.");
+                string input = Console.ReadLine();
+                string username = "";
+                bool correctUser = false;
+                bool correctPass = false;
+
+                foreach (var user in users)
+                {
+                    if (user.Username.Equals(input))
+                    {
+                        currentUser = user;
+                        correctUser = true;
+                        break;
+                    }
+                }
+
+                if (correctUser)
+                {
+                    while ()
+                    {
+                        Console.WriteLine($"God dag {username}. Vänligen skriv ditt lösenord.");
+                        input = Console.ReadLine();
+
+                        if (currentUser.Password.Equals(input))
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Felaktigt lösenord. Tryck på valfri tangent för att försöka igen.");
+                            Console.ReadKey();
+                            Console.Clear();
+                        }
+                    }
+
+                    if(correctPass)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Det finns ingen användare med det användarnamnet. Tryck på valfri " +
+                        "tangent för att gå tillbaka och försöka igen.");
+                    Console.ReadKey();
+                    Console.Clear();
+                }
+            }
         }
     }
 }
