f09ed0d8f791cac837b637ebdcdc28a0568addbf
darkdusk234 <johan.svensson.01@hotmail.com>
2024-11-21 14:23:28 +0100

    Fixed mistake of puttiing varibles into Customer class instead of IUser interface

--- RebelAllianceBank/Classes/Customer.cs
+++ RebelAllianceBank/Classes/Customer.cs
@@ -3,8 +3,6 @@ namespace RebelAllianceBank.Classes
 {
     public class Customer : IUser
     {
-        public string Username { get; set; }
-        public string Password { get; set; }
 
     }
 }
