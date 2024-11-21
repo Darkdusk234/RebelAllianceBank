7d1a3cf5dd9bd91d6c0516cbb4bf2c65883c0188
JosefForkman <strand.vatten@outlook.com>
2024-11-21 21:43:58 +0100

    add showBankAccounts

--- RebelAllianceBank/Classes/Customer.cs
+++ RebelAllianceBank/Classes/Customer.cs
@@ -1,8 +1,35 @@
 ﻿using RebelAllianceBank.Interfaces;
+using RebelAllianceBank.utils;
 namespace RebelAllianceBank.Classes
 {
     public class Customer : IUser
     {
+        private List<IBankAccount> BankAccounts = new List<IBankAccount> {
+            new CardAccount("Card Akount", 7800.56789m, "Kr"),
+            new ISK("ISK", 100000, "USD"),
+            new SavingsAccount("Savings Account", 100000, "$"),
+        };
 
+        public void ShowBankAccounts()
+        {
+            Console.WriteLine("Konton");
+
+            if (BankAccounts.Count == 0)
+            {
+                Console.WriteLine("Du har inga konton att visa");
+            }
+
+           
+            for (int i = 0; i < BankAccounts.Count; i++)
+            {
+                var account = BankAccounts[i];
+                Console.WriteLine($"{account.AccountName} - {account.Balance:00}{account.AccountCurrency}");
+
+            if (BankAccounts.Count - 1 != i)
+            {
+                Console.WriteLine("------------------------------------------");
+            }
+            }
+        }
     }
 }
