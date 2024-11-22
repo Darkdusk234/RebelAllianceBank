d02c1210da2467261b9c6b375e4e6268c102757b
Josef Forkman <44498331+JosefForkman@users.noreply.github.com>
2024-11-22 07:57:16 +0100

    refator Markdown

--- RebelAllianceBank/utils/Markdown.cs
+++ RebelAllianceBank/utils/Markdown.cs
@@ -1,31 +1,37 @@
-using System.Linq;
 using RebelAllianceBank.Interfaces;
 
 namespace RebelAllianceBank.utils;
 
+/// <summary>
+/// This is a helper static class to generate markdown code.
+/// </summary>
 public static class Markdown
 {
 
+    /// <summary>
+    /// This creates a markdown for a table and console out the result
+    /// </summary>
+    /// <param name="headers"></param>
+    /// <param name="body"></param>
     public static void Table(string[] headers, List<string> body)
     {
-        int maxHeaderWidth = headers.OrderByDescending(header => header.Length).First().Length;
+        int maxHeaderWidth = headers.OrderByDescending(item => item.Length).First().Length;
         int maxBodyWith = body.OrderByDescending(item => item.Length).First().Length;
         int maxWith = maxHeaderWidth < maxBodyWith ? maxBodyWith : maxHeaderWidth;
 
-        // Hedder
+        // Table header
         for (int i = 0; i < headers.Length; i++)
         {
             var header = headers[i];
-            int amauntToAddSpce = maxWith - header.Length;
+            int amountToAddSpace = maxWith - header.Length;
 
             Console.Write("|");
             Console.Write(header);
 
-            for (int j = 0; j < amauntToAddSpce; j++)
-            {
-                Console.Write(" ");
-            }
+            // Add remending space on a table cell
+            Spacer(amountToAddSpace);
 
+            // Add the last separator to the end
             if (headers.Length - 1 == i)
             {
                 Console.Write("|");
@@ -36,15 +42,14 @@ public static class Markdown
 
         for (int i = 0; i < headers.Length; i++)
         {
-            int amauntToAddDevider = maxHeaderWidth < 3 ? 3 : maxHeaderWidth;
+            int amountToAddDivider = maxWith < 3 ? 3 : maxWith;
 
             Console.Write("|");
 
-            for (int j = 0; j < maxWith; j++)
-            {
-                Console.Write("-");
-            }
+            // console out the heder separator
+            Spacer(amountToAddDivider, "-");
 
+            // Add the last separator to the end
             if (headers.Length - 1 == i)
             {
                 Console.Write("|");
@@ -53,24 +58,36 @@ public static class Markdown
 
         Console.WriteLine();
 
+        // Table body
         for (int i = 0; i < body.Count; i++)
         {
             Console.Write("|");
             var currentBody = body[i];
-            int count = currentBody.ToString().Length;
-            int amauntToAddSpce = maxWith - count;
+            int amountToAddSpace = maxWith - currentBody.Length;
 
             Console.Write(currentBody);
 
-            for (int j = 0; j < amauntToAddSpce; j++)
-            {
-                Console.Write(" ");
-            }
+            // Add remending space on a table cell
+            Spacer(amountToAddSpace);
 
+            // Add the last separator to the end and add a new row by checking how many item it´s in heder 
             if (((i + 1) % headers.Length) == 0)
             {
                 Console.WriteLine("|");
             }
+        }
+    }
+    
+    /// <summary>
+    /// Add a space withe given amount 
+    /// </summary>
+    /// <param name="amountToAddSpace"></param>
+    /// <param name="spacerValue"></param>
+    private static void Spacer(int amountToAddSpace, string spacerValue = " ")
+    {
+        for (int j = 0; j < amountToAddSpace; j++)
+        {
+            Console.Write(spacerValue);
         }
     }
 }
