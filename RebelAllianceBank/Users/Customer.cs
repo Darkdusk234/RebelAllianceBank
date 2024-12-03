b9289927a151292e422c76d81d23ec883b562530
Kim Andersson <Kimpabooy@live.com>
2024-12-03 11:20:27 +0100

    Loan with rent (#15)
    * Added, customer can now take a loan
    
    * Fixed some bugs in the code.
    
    * leftover stage changes
    
    * - added a quit function to TakeLoan().
    
    * working on the intrest rate for the loans
    
    * Rework of Loan and Customer class

--- RebelAllianceBank/Users/Customer.cs
+++ RebelAllianceBank/Users/Customer.cs
@@ -3,6 +3,7 @@ using RebelAllianceBank.Interfaces;
 using RebelAllianceBank.utils;
 using RebelAllianceBank.Accounts;
 using RebelAllianceBank.Enums;
+using RebelAllianceBank.Classes;
 namespace RebelAllianceBank.Users
 {
     public class Customer : IUser
@@ -13,8 +14,9 @@ namespace RebelAllianceBank.Users
         public string Surname { get; set; }
         public string Forename { get; set; }
         public bool LoginLock { get; set; } = false;
+        
         private List<IBankAccount> _bankAccounts = new List<IBankAccount>();
-
+        private List<decimal> _customerLoan = new List<decimal>();
         public Customer() { }
         public Customer(string pNum, string password, string surname, string forename)
         {
@@ -262,5 +264,169 @@ namespace RebelAllianceBank.Users
 
             return bodyKeys;
         }
+
+        public void TakeLoan()
+        {
+            Loan newLoan = new Loan();
+            bool loanComplete = false;
+            decimal availableToLoan = (MaxAccountBalance() * 5) - MaxCurrentLoan();
+            decimal newLoanTaken = availableToLoan;
+            int choosenAccountIndex = 0;
+
+            while (!loanComplete)
+            {
+                if (_bankAccounts.Count <= 0)
+                {
+                    Console.WriteLine("Du har inga konton. Skapa ett konto först innan du tar ett lån.");
+                    Console.ReadKey();
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine($"Ditt lånetak är: {newLoanTaken} ");
+                    if (newLoanTaken <= 0)
+                    {
+                        Console.WriteLine($"Du kan tyvärr inte låna mer än: {newLoanTaken}.");
+                        Console.ReadKey();
+                        return;
+                    }
+
+                    decimal askedLoan;
+                    Console.Write("\nHur stort lån vill du ansöka om:  ");
+                    if (!decimal.TryParse(Console.ReadLine(), out askedLoan) || askedLoan <= 0)
+                    {
+                        Console.WriteLine("Felaktig inmatning.");
+                        Thread.Sleep(1000);
+                        Console.Clear();
+                        continue;
+                    }
+                    else if (askedLoan > newLoanTaken)
+                    {
+                        Console.WriteLine($"Du kan inte låna mer än {newLoanTaken}.\n"); // known bug, need currency
+                        continue;
+                    }
+
+                    Console.WriteLine("\nVälj vilket konto du vill sätta in pengarna på:\n");
+                    for (int i = 0; i < _bankAccounts.Count; i++)
+                    {
+                        Console.WriteLine($"{i + 1}. {_bankAccounts[i].AccountName}");
+                    }
+                    if (!int.TryParse(Console.ReadLine(), out choosenAccountIndex) || choosenAccountIndex <= 0 || choosenAccountIndex > _bankAccounts.Count)
+                    {
+                        Console.WriteLine("Felaktigt val.");
+                        continue;
+                    }
+
+                    foreach (var account in _bankAccounts)
+                    {
+                        if (account == _bankAccounts[choosenAccountIndex - 1])
+                        {
+                            Console.WriteLine($"Din lånansökan på {askedLoan} {_bankAccounts[choosenAccountIndex - 1].AccountCurrency} har accepterats.\n" +
+                            $"Räntan för detta lån ligger på {newLoan.LoanRent}%");
+                        }
+                    }
+
+                    // Checks if user want to accept the loan with the terms (YES/NO).
+                    if (AcceptLoanTerms())
+                    {
+                        _customerLoan.Add(askedLoan); // add the loan amount to the loanlist.
+                        _bankAccounts[choosenAccountIndex - 1].Balance += askedLoan; // add the loanamount to the account.
+                        newLoanTaken -= askedLoan; // Removes the loanamount from the allowed loan ammount.
+
+                        Console.WriteLine($"{askedLoan} {_bankAccounts[choosenAccountIndex - 1].AccountCurrency}" +
+                                $" har satts in på följande konto: {_bankAccounts[choosenAccountIndex - 1].AccountName}");
+                    }
+                    // checks if customer want to take another loan.
+                    if (!ContinueLoan())
+                    {
+                        loanComplete = true;
+                    }
+                }
+            }
+        }
+
+        public decimal MaxAccountBalance()
+        {
+            decimal maxAccountBalance = 0;
+            foreach (var account in _bankAccounts)
+            {
+                maxAccountBalance += account.Balance;
+            }
+            return maxAccountBalance;
+        }
+
+        public decimal MaxCurrentLoan()
+        {
+            decimal maxCurrentLoans = 0;
+            foreach (var loan in _customerLoan)
+            {
+                maxCurrentLoans += loan;
+            }
+            return maxCurrentLoans;
+        }
+
+        public bool ContinueLoan()
+        {
+            // Checks if customer want to quit the current method or ask for a new loan.
+
+            while (true)
+            {
+                Console.WriteLine("\nVill du ansöka om nytt lån? Ja/Nej");
+                Console.Write("svar:");
+                string quitOrNot = Console.ReadLine().ToLower(); // Set ReadKey to true to remove the input text in the consol.
+                Console.Clear();
+                if (quitOrNot == "nej" || quitOrNot == "n")
+                {
+                    Console.WriteLine("\nDu har valt att avsluta låneansökan.");
+                    Console.WriteLine("Tryck på valfri tangent för att avsluta.");
+                    Console.ReadKey();
+                    return false;
+                }
+                else if (quitOrNot == "ja" || quitOrNot == "j")
+                {
+                    Console.WriteLine("Går vidare med en ny ansökan.");
+                    Thread.Sleep(2000);
+                    Console.Clear();
+                    return true;
+                }
+                else
+                {
+                    continue;
+                }
+
+            }
+        }
+
+        public bool AcceptLoanTerms()
+        {
+
+            while (true)
+            {
+                // Checks if user want to accept the loan with the terms (YES/NO).
+                Console.Write("\nAccepterar du villkoren för detta lån? Ja/Nej");
+                Console.Write("\nSvar: ");
+                string terms = Console.ReadLine().ToLower();
+                if (terms == "ja" || terms == "j")
+                {
+                    return true;
+                }
+                else if (terms == "nej" || terms == "n")
+                {
+                    Console.WriteLine("Lånet har avbrutits...");
+                    Thread.Sleep(1500);
+                    Console.Clear();
+                    return false;
+                }
+                else
+                {
+                    Console.WriteLine("Hint: Testa med ett \"J\" eller \"N\".");
+                    Thread.Sleep(1500);
+                    continue;
+                }
+            }
+        }
+
+
+
     }
 }
