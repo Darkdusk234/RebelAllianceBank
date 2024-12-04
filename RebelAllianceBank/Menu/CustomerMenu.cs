7db2d06f9d55b97a436d32fb35cb4c70d2fc3e93
darkdusk234 <johan.svensson.01@hotmail.com>
2024-12-04 10:49:29 +0100

    Removed unnessecary Console.ReadKey

--- RebelAllianceBank/Menu/CustomerMenu.cs
+++ RebelAllianceBank/Menu/CustomerMenu.cs
@@ -90,12 +90,11 @@ namespace RebelAllianceBank.Menu
                     case 0:
                         Console.Clear();
                         _currentCustomer.ShowBankAccounts();
-                        Console.ReadKey();
+                        Console.ReadKey(); //Think about moving into ShowBankAccounts method
                         break;
                     case 1:
                         Console.Clear();
                         _currentCustomer.CreateAccount();
-                        Console.ReadKey(); //Think about moving into CreateAccount method
                         break;
                     case 2:
                         runCustomerMenuAccounts = false;
